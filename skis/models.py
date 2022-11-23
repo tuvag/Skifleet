36f2c5da93e776fcb1833f3593b0740132b9971a
chezmarcbrown <marc@chezbrown.org>
2022-11-22 16:19:51 -0900

    commets on model

--- skis/models.py
+++ skis/models.py
@@ -7,20 +7,39 @@ class User(AbstractUser):
 
 
 class Ski(models.Model):
+    # ski_number: is this a unique number? if so, add "unique=true". it could also be 
+    # used as the primary key, but 
     ski_number = models.CharField(max_length = 64)
+
+    # technique: do you want the technique to be a FK to the Technique class? 
     technique = models.CharField(max_length = 64)
+
+    # grind, brand: should these be FKs? (if it's easier for now to have this be a string,
+    # keep it that way for the time being)
     grind = models.CharField(max_length = 64)
     brand = models.CharField(max_length = 64)
+
     img = models.ImageField(upload_to='images/', blank=True)
     url_img = models.URLField()
+
+    # is this an RGB value? 
     color_tag = models.IntegerField()
+
     notes = models.CharField(max_length = 256)
-    ski_owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="owner")
+
+    # the related name should be "skis_owned". If you have given a user, you
+    # can get all the skis that she owns by saying "user.skis_owned.all()" or 
+    # you can filter based on, say, grind: "user.skis_owner.filter(grind="blahblah").
+    ski_owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="skies_owned")
 
 class Technique(models.Model):
     technique = models.CharField(max_length=64)
 
-class Skitest(models.Model):
+# super confusing to have Skitest and Testski. Can you think of a better name
+# for both? SkiTest is something like "Setting". And then "Testski" is think would
+# be best called "SkiTest". Don't start a class with "Test"; it's magic to django. 
+
+class Setting(models.Model):
     date_of_test = models.DateField()
     temprature = models.IntegerField()
     humidity = models.IntegerField(null=False)
@@ -28,10 +47,10 @@ class Skitest(models.Model):
     snow_type = models.CharField(max_length = 64)
     notes = models.CharField(max_length= 256)
     testski = models.ManyToManyField(Ski, through='Testski')
-    tester = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="tester")
+    tester = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="skies_tested")
 
-class Testski(models.Model):
-    ski = models.ForeignKey(Ski, on_delete=models.CASCADE, blank=True, null=True, related_name="testski")
-    test = models.ForeignKey(Skitest, on_delete=models.CASCADE, blank=True, null=True, related_name="test")
+class SkiTest(models.Model):
+    ski = models.ForeignKey(Ski, on_delete=models.CASCADE, blank=True, null=True, related_name="tests")
+    setting = models.ForeignKey(Skitest, on_delete=models.CASCADE, blank=True, null=True, related_name="tests")
     rank = models.IntegerField()
 
